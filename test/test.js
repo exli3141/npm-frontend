73ff95c9231f57f638fbe841cc6b60643e97713f
exli3141 <exli3141@gmail.com>
2018-01-24 13:36:33 +0900

    karmaによるテスト環境の実装。

--- test/test.js
+++ test/test.js
@@ -2,16 +2,16 @@
 
 //nodeのassertionテスト。
 const assert = require('assert');
-const hello = require('../src/hello');
-const World = require('../src/world');
+import hello from '../src/hello';
+import World from '../src/world';
 
 describe('App', () => {
   describe('Hello world', () => {
     it('Helloクラスはh,e,l,l,oという5つの配列です。また、あえてNewせずとも自動でインスタンスが作られます。', () => {
-      assert(equal(['H', 'e', 'l', 'l', 'o'], hello.message));
+      assert.deepEqual(['H','e','l','l','o'], hello.message);
     });
     it('Worldクラスは投げ込まれた配列を結合して返すメソッドのみを持っています。', () => {
-      assert(equal('World'), new World(['W', 'o', 'r', 'l', 'd']));
+      assert.deepEqual('World', new World(['W', 'o', 'r', 'l', 'd']).message);
     });
   });
 });
